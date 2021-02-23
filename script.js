04800e6f0acb50468dcf8f51911291068a82fb0c
mountaindew2141@outlook.com <mountaindew2141@outlook.com>
2021-02-22 23:34:05 -0500

    added script.js and added Synth Hero game

---
+++ script.js
@@ -0,0 +1,19 @@
+var str = "Hello Developers!"; 
+console.log(str); 
+
+
+var studentName;
+var studentName = "Placeholder";
+var studentAge = 32;
+
+studentName = "Here";
+studentAge = 52;
+
+console.log(studentName);
+console.log("is an updated version of ");
+
+studentName="my"
+
+var studentName;
+
+console.log("Wakko's World");
